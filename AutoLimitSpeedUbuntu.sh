061bfb645fefb93b0e3695f6af50e5925993fdad
PR Aiman <58582776+praiman99@users.noreply.github.com>
2024-08-08 14:03:22 +0800

    Update AutoLimitSpeedUbuntu.sh

--- AutoLimitSpeedUbuntu.sh
+++ AutoLimitSpeedUbuntu.sh
@@ -9,6 +9,6 @@ cd /usr/sbin
 wget -O limit "https://github.com/ingnelson/Auto-Limit-Speed-Bandwith-VPS/raw/Beginner/limit-speed.sh"; chmod +x limit; bash limit
 
 # Add Limit Speed At Crontab
-echo "30 6 * * * root bash limit" >> /etc/Crontab
+echo "30 6 * * * root bash limit" >> /etc/crontab
 
 rm -f AutoLimitSpeedUbuntu.sh
